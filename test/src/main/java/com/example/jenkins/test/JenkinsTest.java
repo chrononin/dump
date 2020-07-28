060de87d43e3c53a8eca8f32037149f57dc7f1d4
pradip <5646743+chrononin@users.noreply.github.com>
2020-07-28 15:56:33 +0530

    initial commit

---
+++ test/src/main/java/com/example/jenkins/test/JenkinsTest.java
@@ -0,0 +1,10 @@
+package com.example.jenkins.test;
+
+public class JenkinsTest {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Jenkins test Dashboard!");
+
+	}
+
+}
---
+++ test/src/test/java/com/example/jenkins/test/JenkinsTester.java
@@ -0,0 +1,12 @@
+package com.example.jenkins.test;
+
+import org.junit.jupiter.api.Test;
+
+public class JenkinsTester {
+	
+	@Test
+	public void run() {
+		System.out.println("Welcome to the Jenkins dashboard page!");
+	}
+
+}
